4a39e2a3cafa750bb2430d5d8e0854a0e9968498
ArtemKorole <artemrodilsya24092009@gmail.com>
2024-11-10 14:36:57 +0300

    remake styles and car info display

--- front/src/components/AdsList/AdsList.jsx
+++ front/src/components/AdsList/AdsList.jsx
@@ -33,21 +33,14 @@ const AdsList = () => {
           ads.map((ad) => (
             <div key={ad.id} className="ad">
               <img className="car-image" src={ad.car.image_url.url} alt={ad.car.image_url.url} />
-              <h3>{ad.car.name}</h3>
-              <p>{ad.car.model.name}</p>
-              <p>Объем двигателя: {ad.car.volume} л.куб.</p>
-              <p>Пробег: {ad.car.mileage} км</p>
-              <p>Мощность: {ad.car.power} л.с</p>
-              <p>Тип кузова: {ad.car.body}</p>
-              <p>Тип трансмиссии: {ad.car.transmission}</p>
-              <p>Цвет: {ad.car.color}</p>
-              <p>Тип привода: {ad.car.drive}</p>
-              <p>
-                <strong>Цена:</strong> {ad.car.price}₽
-              </p>
-              <p>
-                Владелец: {ad.user.name} ({ad.user.contact_number})
-              </p>
+              <div className="car-info">
+                <p>{ad.car.model.name} {ad.car.name} {ad.car.transmission}</p>
+                <p>{ad.car.mileage} км</p>
+                <p>{ad.car.body}</p>
+                <p>{ad.car.drive}</p>
+                <p>{ad.car.volume} ({ad.car.power} л.с)</p>
+                <p className="car-price">{ad.car.price} ₽</p>
+              </div>
             </div>
           ))
         ) : (
